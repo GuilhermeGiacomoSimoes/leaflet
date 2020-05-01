493f105d06ccc538d0fdff746f57d032956dcd81
Guilherme <trintaeoitogc@gmail.com>
2020-05-01 12:58:34 -0300

    fix mapp

--- leaflet.js
+++
@@ -1,8 +0,0 @@
-const initialCoordinates = [212060071, -504380251];
-const initialZoomLevel = 13;
-
-var map = L.map('map').setView(initialCoordinates, initialZoomLevel);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; Contribuidores do <a href="http://osm.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
