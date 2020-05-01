3918ec424986843fd203def990d0f0b50cbabd06
Guilherme <trintaeoitogc@gmail.com>
2020-05-01 13:55:51 -0300

    mapping

--- map.js
+++ map.js
@@ -1,11 +1,11 @@
-const coordinates = [51.505, -0.09];
-const zoomDefault = 13;
+const coordinates = [-21.206288, -50.438914];
+const zoomDefault = 20;
 
 const mymap = L.map('mapid').setView(coordinates, zoomDefault);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?' + 
             'access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom:     18,
+    maxZoom:     20,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                  '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
                  'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,20 +14,21 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?' +
     zoomOffset:  -1
 }).addTo(mymap);
 
-L.marker([51.5, -0.09]).addTo(mymap).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+L.marker([-21.239985, -50.405172])
+    .addTo(mymap)
+    .bindPopup("<b>Hello world!</b><br />I am a gas station.")
+    .openPopup();
 
-L.circle([51.508, -0.11], 500, {
+L.circle([-21.235222, -50.407101], 100, {
     color:      'red',
     fillColor:  '#f03',
     fillOpacity: 0.5
 }).addTo(mymap).bindPopup("I am a circle.");
 
 L.polygon([
-    [51.509, -0.080],
-    [51.503, -0.060],
-    [51.51,  -0.047]
+    [-21.206288, -50.438914],
+    [-21.236112, -50.408834],
 ]).addTo(mymap).bindPopup("I am a polygon.");
-
 
 var popup = L.popup();
 
